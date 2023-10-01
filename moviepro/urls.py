007fbee10cb15093a800c7424b9453458b22264b
Eze Moses <ezemosesjohn21997@gmail.com>
2023-09-30 22:38:53 -0500

    made changes

--- moviepro/urls.py
+++ moviepro/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('contacts', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
